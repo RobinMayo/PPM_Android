03891e1d4f957fc0409157b482961c917bcf635d
RobinMayo <rbmayo@laposte.net>
2019-02-11 03:18:08 +0100

    Improve collisions detection.

--- app/src/main/java/com/robinmayo/crossingroads/GameView.java
+++ app/src/main/java/com/robinmayo/crossingroads/GameView.java
@@ -8,6 +8,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -23,7 +24,7 @@ public class GameView extends View {
     private Bitmap carToRight;
     private static int carWidth;
     private static int carHeight;
-    private int score;
+    private int life;
     private int playerX;
     private int playerY;
     private static int playerWidth;
@@ -32,11 +33,13 @@ public class GameView extends View {
     private int car1X, car1Y;
     private int car2X, car2Y;
     private int carSpeed;
+    private int difficulty;
 
     private boolean touch = false;
 
     private Bitmap backgroundImage;
     private Paint scorePaint = new Paint();
+    private Paint levelPaint = new Paint();
 
 
     public GameView(Context context) {
@@ -49,6 +52,7 @@ public class GameView extends View {
 
         int width = Resources.getSystem().getDisplayMetrics().widthPixels;
         int height = Resources.getSystem().getDisplayMetrics().heightPixels;
+        this.difficulty = difficulty;
 
         Bitmap smurf = BitmapFactory.decodeResource(getResources(), R.drawable.smurf);
         playerInSize = Bitmap.createScaledBitmap(smurf, width / 5,
@@ -59,8 +63,8 @@ public class GameView extends View {
         Bitmap car1InSize = BitmapFactory.decodeFile(toRightCar.getPath());
         carToLeft = Bitmap.createScaledBitmap(car1InSize, width / 5,
                 height /5, true);
-        carWidth = carToLeft.getWidth();
-        carHeight = carToLeft.getHeight();
+        carWidth = (int) (carToLeft.getWidth() / 0.9);
+        carHeight = (int) (carToLeft.getHeight() / 0.9);
 
         Bitmap car2InSize = BitmapFactory.decodeFile(toLeftCar.getPath());
         carToRight = Bitmap.createScaledBitmap(car2InSize, width / 5,
@@ -69,10 +73,15 @@ public class GameView extends View {
         Bitmap backgroundInSize = BitmapFactory.decodeFile(background.getPath());
         backgroundImage = Bitmap.createScaledBitmap(backgroundInSize, width, height, true);
 
-        scorePaint.setColor(Color.YELLOW);
+        scorePaint.setColor(Color.BLACK);
         scorePaint.setTextSize((float)height / 20);
         scorePaint.setTypeface(Typeface.DEFAULT_BOLD);
         scorePaint.setAntiAlias(true);
+
+        levelPaint.setColor(Color.BLACK);
+        levelPaint.setTextSize((float)height / 20);
+        levelPaint.setTypeface(Typeface.DEFAULT_BOLD);
+        levelPaint.setAntiAlias(true);
 
         minPlayerX = 0;
         maxPlayerX = width - playerInSize.getWidth();
@@ -84,15 +93,17 @@ public class GameView extends View {
         playerX = (height / 2) - (playerInSize.getHeight() / 2);
 
         carSpeed = difficulty;
-        score = 0;
+        life = 3;
     }
 
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
         int w = this.getWidth();
         //int h = this.getHeight();
+        int margine = 30;
 
         if (playerY > maxPlayerY) {
             playerY = maxPlayerY;
@@ -108,16 +119,20 @@ public class GameView extends View {
         }
 
         if (collision(car1X, car1Y)) {
-            score = score + 1;
-            car1X = 99999;
+            life = life - 1;
+            car1X = 0;
         }
         if (collision(car2X, car2Y)) {
-            score = score + 1;
-            car2X = -100;
+            life = life - 1;
+            car2X = w;
         }
 
         canvas.drawBitmap(backgroundImage, 0, 0, null);
-        canvas.drawText("Score : " + score, 10, 30, scorePaint);
+        String levelName = "Level : " + difficulty;
+        String remainingLives = "Life : " + life;
+        canvas.drawText(levelName, margine, margine, scorePaint);
+        canvas.drawText(remainingLives, w - scorePaint.measureText(remainingLives), margine,
+                scorePaint);
 
         if(touch) {
             canvas.drawBitmap(playerInSize, playerX, playerY,null);
@@ -146,12 +161,16 @@ public class GameView extends View {
         boolean playerIsOnTheLeft   = playerX > (carX + carWidth);
         boolean playerIsOnTheTop    = (playerY + playerHeight) > carY;
         boolean playerIsDownstairs  = playerY < (carY + carHeight);
-        boolean playerIsOnCarLine   = playerY > carY && playerY < (carY + carHeight);
-        boolean playerIsOnCarColumn = playerX > carX && playerX < (carX + carWidth);
+        boolean playerIsOnCarLine   = (playerY > carY && playerY < (carY + carHeight))
+                || ((playerY + carHeight) > carY && (playerY + carHeight) < (carY + carHeight));
+        boolean playerIsOnCarColumn = (playerX > carX && playerX < (carX + carWidth))
+                || ((playerX + playerWidth) > carX && (playerX + playerWidth) < (carX + carWidth));
 
         if (playerIsOnCarLine) {
+            //Log.d(TAG, "collision(...) - playerIsOnCarLine");
             return !(playerIsOnTheRight || playerIsOnTheLeft);
         } else if (playerIsOnCarColumn) {
+            Log.d(TAG, "collision(...) - playerIsOnCarColumn");
             return !(playerIsOnTheTop   || playerIsDownstairs);
         }
         return false;
@@ -161,8 +180,8 @@ public class GameView extends View {
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_MOVE ||
                 event.getAction() == MotionEvent.ACTION_DOWN) {
-            playerX = (int)(event.getRawX() - playerInSize.getWidth() / 2);
-            playerY = (int)(event.getRawY() - playerInSize.getHeight() * 0.50);
+            playerX = (int)(event.getRawX() - playerInSize.getWidth() * 0.5);
+            playerY = (int)(event.getRawY() - playerInSize.getHeight() * 0.20);
         }
         return true;
     }
